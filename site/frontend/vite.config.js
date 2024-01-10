60cf4beafe0bd1d18e4331b9b76f224ade708a4e
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2024-01-10 11:57:19 +0530

    Hey

--- site/frontend/vite.config.js
+++ site/frontend/vite.config.js
@@ -8,16 +8,26 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest:  {
-        "name": "Aquila Enterprise",
-        "icons": [
+      manifest: {
+        "registerType": 'autoUpdate',
+        "name":"Aquila Enterprise",
+        "short_name":"Aquila",
+        "display": "standalone",
+        "background_color": "#141E46",
+        "theme_color": "#141E46",
+        "icons":[
           {
             src: "Business/Aquila/favicon_io/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          }
+          },
+          {
+            src: "Business/Aquila/favicon_io/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
         ],
-      } 
+      }
     })
   ],
  
